e62e8786df1ccc893206762ed99fb571c99e92ee
Migu3lito5 <mfrancisco@csumb.edu>
2022-07-29 17:31:33 -0700

    Finished basic functionality of todo app

--- scripts/todo-script.js
+++ scripts/todo-script.js
@@ -1,16 +1,9 @@
-let todoList = [
-   {  
-      todoDesc: 'this is a todo description',
-      priority: 'its either 1,2,3'
-   }
-
-]
+let todoList = []
 
 // adds the todo to the array and starts the displayTodo function
 const addTodo = (todo,urgency) => {
 
-   todoList.push({todoDesc: todo, priority: urgency})
-
+   todoList.push({todoDesc: todo, priority: urgency, date: Date()})
    displayTodos()
    
 }
@@ -18,23 +11,75 @@ const addTodo = (todo,urgency) => {
 
 // delete todo from array and call display todo
 const deleteTodo = () => {
+  let idToDelete = event.srcElement.id;
+  
 
+   todoList = todoList.filter( i => {
+    if (i.date == idToDelete)
+      return false;
+    else 
+      return true;
+  });
+
+  displayTodos()
+  
 }
 
 // displays todoList onto the screen
 const displayTodos = () => {
 
-   const todoSection = document.getElementById('todo-container');
-   todoSection.innerHTML = '';
+   let divContainer = document.getElementById('todo-container');
+   divContainer.innerHTML = '';
+
+ 
 
    todoList.forEach(i => {
-      const todoDiv = document.createElement('div');
-      let todoText = document.createElement('p');
-      todoText.textContent = i.todoDesc
+      let todoRow = document.createElement('div');
+      let todoDesc = document.createElement('p');
+      let todoButton = document.createElement('button');
+      assignClassNames(todoRow, todoDesc, todoButton, i)
 
-      todoDiv.appendChild(todoText);
-      todoSection.appendChild(todoDiv);
+      todoDesc.textContent = i.todoDesc;
+      assignColor(todoRow, i)
+      todoRow.appendChild(todoDesc);
+      todoRow.appendChild(todoButton);
+
+      divContainer.appendChild(todoRow);
+      
    })
+
+}
+
+const sortTodosByPriority = () => {
+
+     todoList.sort(function(x,y){
+      return x.priority - y.priority;
+   });
+
+   displayTodos();
+}
+
+const assignColor = (todoRow, i) => {
+
+   if (i.priority == 1) 
+      todoRow.style.backgroundColor = 'rgb(' + 207 + ',' + 162 + ',' + 157 + ')';
+   else if (i.priority == 2){
+      todoRow.style.backgroundColor = 'rgb(' + 170 + ',' + 207 + ',' + 157 + ')';
+   }
+   else {
+      todoRow.style.backgroundColor = 'rgb(' + 157 + ',' + 189 + ',' + 207 + ')';
+   }
+
+}
+
+
+const assignClassNames = (todoRow, todoDesc, todoButton, i) => {
+   todoRow.classList.add('todo-row')
+   todoDesc.classList.add('todo-desc')
+   todoButton.classList.add('todo-button')
+   todoButton.setAttribute('onClick', 'deleteTodo()')
+   todoButton.innerText = 'X'
+   todoButton.setAttribute('id', i.date)
 
 }
 
@@ -43,7 +88,7 @@ const grabInput = () => {
 
    let inputValue = document.getElementById('todo-input').value
    let urgentType = document.getElementById('urgency')
-   let urgentValue = urgentType.options[urgentType.selectedIndex].text;
+   let urgentValue = urgentType.options[urgentType.selectedIndex].value;
 
    addTodo(inputValue, urgentValue);
 }
